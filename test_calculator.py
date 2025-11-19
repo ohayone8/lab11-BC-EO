03efc497c23fe55b20bf2f6c3a614cb81a0b92b2
eohayon8 <reacheden8@gmail.com>
2025-11-19 12:56:58 -0500

    P2 calculator tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,21 @@
+#https://github.com/ohayone8/lab11-BC-EO.git
+#Partner 1: Bianca Claro
+#Partner 2: Eden Ohayon
+
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2,-3), -1)
+        self.assertEqual(add(-2,-3), -5)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(subtract(2, 3), -1)
+        self.assertEqual(subtract(2, -3), 5)
+        self.assertEqual(subtract(-2, -3), 1)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,19 +26,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
+        self.assertEqual(div(2,10), 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(logarithm(2, 4), 2)
+        self.assertEqual(logarithm(10, 100), 2)
+        self.assertEqual(logarithm(8, 8), 1)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(0, 5)
+        self.assertEqual(logarithm(2, 8), 3)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
